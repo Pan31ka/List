9165c09e17aae29db64148c720abc074334fe194
Pan31ka <xoxlov789@gmil.com>
2021-06-05 22:17:55 +0300

    List To do

--- src/state.js
+++ src/state.js
@@ -1,23 +1,24 @@
-let StateList = {	
+	let store = {
 	
-		ListItem: [
-			{id:1,product:'Meat'},
-			{id:2,product:'Pen'},
-			{id:3,product:'Water'},
-			{id:4,product:'Aple'},
-			{id:5,product:'Banana'},
-			{id:6,product:'Orange'},
-			{id:7,product:'Juice'},
-		]
-	
-}
-export default StateList
-/*
-let store = {
-	_state: {
+	onChange(id) {
+		return(
+			console.log('id')
+		)
+	},
 
-		PostsPage: {
-			MyPostData: [
-				{ id: 1, text: 'hi, how are you', like: '15' },
-				{ id: 2, text: 'it my first project', like: '351' }
-			]}}}*/
+
+
+		
+		
+			ListItem: [
+				{id:1,product:'Meat'},
+				{id:2,product:'Pen'},
+				{id:3,product:'Water'},
+				{id:4,product:'Aple'},
+				{id:5,product:'Banana'},
+				{id:6,product:'Orange'},
+				{id:7,product:'Juice'},
+			]
+		
+	}
+export default store
